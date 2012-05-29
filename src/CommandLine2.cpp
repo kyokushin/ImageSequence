cd4de15f6879e93cac435abb062367c62a54ce6d
yuta suzuki <kyokushin@vmware.centos6-32bit-dev>
2012-05-30 06:26:13 +0900

    CommandLine2クラスの実装完了。次はテストの作成

--- src/CommandLine2.cpp
+++ src/CommandLine2.cpp
@@ -6,10 +6,11 @@
 using namespace std;
 using namespace ys;
 
-int OptionSetting::count = 0;
+
+int OptionSetting::_count = 0;
 
 bool CommandLine::_checkRefCount( const std::string &opt ){
-	int ref_num = _options_value[opt].first;
+	int ref_num = _options_value[opt].second;
 	if( ref_num != 0 && ref_num != 1 ){
 		cerr<< "not setting option > " << opt <<endl;
 		return false;
@@ -18,15 +19,15 @@ bool CommandLine::_checkRefCount( const std::string &opt ){
 
 }
 
-void CommandLine::addOption( const std::string &opt,  ){
-	_options_setting[opt] = pair<int,string>(0,"");
+void CommandLine::addOption( const std::string &opt, bool has_value, char alias ){
+	_options.addOption( opt, has_value, alias );
 }
 
 char CommandLine::getChar( const std::string &opt ){
 	if( _checkRefCount(opt) ) return 0;
 
 	char tmp;
-	stringstream sstr(_options_value[opt].second);
+	stringstream sstr(_options_value[opt].first);
 	sstr>> tmp;
 	return tmp;
 }
@@ -34,7 +35,7 @@ unsigned char CommandLine::getUChar( const std::string &opt ){
 	if( _checkRefCount(opt) ) return 0;
 
 	unsigned char tmp;
-	stringstream sstr(_options_value[opt].second);
+	stringstream sstr(_options_value[opt].first);
 	sstr>> tmp;
 	return tmp;
 }
@@ -42,7 +43,7 @@ int CommandLine::getInt( const std::string &opt ){
 	if( _checkRefCount(opt) ) return 0;
 
 	int tmp;
-	stringstream sstr(_options_value[opt].second);
+	stringstream sstr(_options_value[opt].first);
 	sstr>> tmp;
 	return tmp;
 }
@@ -50,7 +51,7 @@ float CommandLine::getFloat( const std::string &opt ){
 	if( _checkRefCount(opt) ) return 0;
 
 	float tmp;
-	stringstream sstr(_options_value[opt].second);
+	stringstream sstr(_options_value[opt].first);
 	sstr>> tmp;
 	return tmp;
 }
@@ -58,27 +59,31 @@ double CommandLine::getDouble( const std::string &opt ){
 	if( _checkRefCount(opt) ) return 0;
 
 	double tmp;
-	stringstream sstr(_options_value[opt].second);
+	stringstream sstr(_options_value[opt].first);
 	sstr>> tmp;
 	return tmp;
 }
 string& CommandLine::getString( const std::string &opt ){
 	if( _checkRefCount(opt) ) return 0;
 
-	return _options_value[opt].second;
+	return _options_value[opt].first;
 }
 
-//TODO:getopt_longを使ったオプション解析
 void CommandLine::parse( int argc, char **argv ){
+
 	option *long_opts = new option[ _options_setting.size() + 1 ];
 	int count = 0;
 	string opt_list;
-	for(vector<Option>::iterator itr = _options.begin();
-			itr != _options.end(); itr++){
-		Option tmp_opt = itr->first;
+
+	for( OptionSettingAliasMap::iterator itr = _options_setting.begin();
+			itr != _options_setting.end(); itr++){
+
+		OptionSetting tmp_opt = itr->first;
+
+		//getopt_longの設定
 		option opt = { tmp_opt.name.c_str(), tmp_opt.has_value, NULL, tmp_opt.alias };
 		long_opts[ count ] = opt;
-		if( 'a' <= tmp_opt.alias && tmp_opt.alias <= 'Z' )
+		if( 0 <= tmp_opt.alias && tmp_opt.alias <= 255 )
 			opt_list += tmp_opt.alias;
 		if( tmp_opt.has_value )
 			opt_list += ':';
@@ -87,6 +92,13 @@ void CommandLine::parse( int argc, char **argv ){
 	}
 
 	long_opts[_options.size()]  = {0,0,0,0};
+
+	int opt_index = 0;
+	while( (int result = getopt_long(argc, argv, opt_list.c_str(), long_opts, &opt_index)) != -1 ){
+		OptionSetting tmp_opt = _options[(char)result];
+		_options_value[tmp_opt._name] = pair<string,int>(optarg,0);
+	}
+
 
 	delete long_opts;
 }
--- src/CommandLine2.h
+++ src/CommandLine2.h
@@ -7,33 +7,34 @@
 namespace ys {
 	class OptionSetting{
 		public:
-			OptionSetting(bool has_value = true, char alias = -1)
-				:has_value(has_value) {
-					if( alias == -1 ){
-						this->alias = count;
-						count++;
+			OptionSetting( std::string name, bool has_value = true, char alias = -1)
+				:_name(name),_has_value(has_value) {
+					if( _alias == -1 ){
+						this->_alias = _count;
+						_count++;
 					}
 					else {
 						this->alias = alias;
 					}
 				}
-		bool has_value;
-		char alias;
-		static int count;
+		std::string _name;
+		bool _has_value;
+		char _alias;
+		static int _count;
 	}
-	class OptionsSetting : std::map<std::string, OptionSetting> {
+	class OptionSettingAliasMap : std::map<char, OptionSetting> {
 		public:
-			void addOption( std::string name, bool has_value = true, char alias = -1 );
-			std::string name;
-			bool has_value;
-			char alias;
+			void addOption( std::string name, bool has_value = true, char alias = -1 ){
+				OptionSetting opt( name, has_value, alias );
+				(*this)[opt._alias] = opt;
+			}
 	}
 
 	class CommandLine {
 		public:
-			CommandLine( OptionsSetting &setting );
+			CommandLine();
 			void setOptionSetting( OptionsSetting &setting );
-			void addOption( const std::string &opt, bool has_value = true, char alias = 0 );
+			void addOption( const std::string &opt, bool has_value = true, char alias = -1 );
 			unsigned char getUChar( const std::string &opt );
 			char getChar( const std::string &opt );
 			int getInt( const std::string &opt );
@@ -43,8 +44,11 @@ namespace ys {
 			void parse( int argc, char**argv );
 
 		private:
-			OptionsSetting _options_setting;
-			std::map<char, std::pair< int, std::string> > _options_value;
+			OptionSettingAliasMap _options;
+			//first:OptionSettingAliasMap,
+			//second.first:option_name
+			//second.second:reference_count
+			std::map<std::string, std::pair< std::string, int> > _options_value;
 			bool _checkRefCount( const std::string &opt );
 	}
 
